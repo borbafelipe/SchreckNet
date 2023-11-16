f9e562eeb321f38a375f7885c5899e27f99b81e3
borbafelipe <149276247+borbafelipe@users.noreply.github.com>
2023-11-16 20:53:57 -0300

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
+
 // functions.php
+session_start();
 
 function conectarBanco() {
     $dbhost = 'localhost';
@@ -25,4 +27,37 @@ function obterContadorComentarios($conn, $post_id) {
     
     return $result;
 }
+
+function getPostById($post_id, $conn) {
+    $sql = "SELECT * FROM posts WHERE id = :post_id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':post_id', $post_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $result;
+}
+
+function getCommentsByPostId($post_id, $conn) {
+    $sql = "SELECT * FROM comments WHERE post_id = :post_id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':post_id', $post_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $result;
+}
+
+function addComment($conn, $post_id, $username, $message) {
+    $sql = "INSERT INTO comments (post_id, username, message) VALUES (:post_id, :username, :message)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':post_id', $post_id, PDO::PARAM_INT);
+    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+    $stmt->bindParam(':message', $message, PDO::PARAM_STR);
+    $stmt->execute();
+}
+
+// Restante do código...
+
 ?>
+
